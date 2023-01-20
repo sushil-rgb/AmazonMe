081e0bc6f48cb5d5b5cb169f729c7176bd6db615
sushil-rgb <sushil.bhandari002@gmail.com>
2023-01-21 01:11:14 +0545

    added functionalities

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from tools import amazonMe
 start_time = time.time()
 
 # Make False if you want to see the live automation.
-make_headless = False
+make_headless = True
 
 amazonMe(make_headless)
 
@@ -15,4 +15,3 @@ time_in_mins = round(total_time/60)
 
 print(f"Took {time_in_secs} seconds | {time_in_mins} minutes.")
 
-  
--- tools.py
+++ tools.py
@@ -40,10 +40,12 @@ def amazonMe(head):
     amazon_dicts = []
     catchClause = TryExcept()
 
-    user_input = str(input("Enter a URL:> "))    
-    amazon_link_pattern = re.search("^https://www.amazon.com/s\?k=.*", user_input)
-    if amazon_link_pattern != None:
-        print(f"Invalid link. Please try proper Amazon link.")
+    user_input = str(input("Enter a URL:> "))   
+    
+    # regex pattern to verify if the entered link is correct Amazon link:
+    amazon_link_pattern = re.search("^https://www.amazon.com/s\?.+", user_input)
+    if amazon_link_pattern == None:
+        print(f"Invalid link. Please enter an amazon link including product category of your choice.")
         sys.exit()
 
     with sync_playwright() as play:
@@ -54,7 +56,12 @@ def amazonMe(head):
         page.wait_for_timeout(timeout=randomTime(4)*1000)
 
         ##################### XPATH selectors ###########################################################################################################
-        search_name = "//span[@class='a-color-state a-text-bold']"
+        # Below variable is for the searched product, there could be more that two elements for it.
+        try:
+            product_name = page.query_selector("//div[@id='departments']/ul[@class='a-unordered-list a-nostyle a-vertical a-spacing-medium']/li[@class='a-spacing-micro s-navigation-indent-1']/span[@class='a-list-item']/span[@class='a-size-base a-color-base a-text-bold']").inner_text().strip()
+        except AttributeError:
+            product_name = page.query_selector("//div[@id='departments']/ul[@class='a-unordered-list a-nostyle a-vertical a-spacing-medium']/li[@class='a-spacing-micro']/span[@class='a-list-item']").inner_text().strip()
+        
         total_page_number_first = "//span[@class='s-pagination-item s-pagination-disabled']"
         total_page_number_second = "//span[@class='s-pagination-strip']/a"
         next_button = "//a[@class='s-pagination-item s-pagination-next s-pagination-button s-pagination-separator']"
@@ -68,15 +75,13 @@ def amazonMe(head):
         review_count = "//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style']/span[@class='a-size-base s-underline-text']"
         image = "//img[@class='s-image']"
         ###################################################################################################################################################
-
-        product_name = re.sub(
-            r"[^a-zA-Z0-9]", "", catchClause.text(page.query_selector(search_name))).capitalize()
-
+        print(product_name)
+        
         try:
             page.wait_for_selector(main_content, timeout=10*1000)
         except PlaywrightTimeoutError:
-            print(f"Content loading error. Please try again in few minute.")
-
+            print(f"Content loading error. Please try again in few minute.")        
+        
         try:
             last_page = page.query_selector(
                 total_page_number_first).inner_text().strip()
@@ -86,9 +91,9 @@ def amazonMe(head):
         print(f"Number of pages | {last_page}.")
         print(f"Scraping | {product_name}.")
 
-        for click in range(int(last_page)):
-            print(f"Scraping page | {click+1}")
-            page.wait_for_timeout(timeout=randomTime(5)*1000)
+        for click in range(1, int(last_page)):
+            print(f"Scraping page | {click}")
+            page.wait_for_timeout(timeout=randomTime(8)*1000)
             for content in page.query_selector_all(main_content):
                 data = {
                     "Product": catchClause.text(content.query_selector(hyperlink)),
@@ -97,7 +102,7 @@ def amazonMe(head):
                     "Original price": catchClause.text(content.query_selector(old_price)),
                     "Review": catchClause.text(content.query_selector(review)),
                     "Review count": re.sub(r"[()]", "", catchClause.text(content.query_selector(review_count))),
-                    "Hyperlinnk": f"""http://www.amazon.com{catchClause.attributes(content.query_selector(hyperlink), 'href')}""",
+                    "Hyperlink": f"""http://www.amazon.com{catchClause.attributes(content.query_selector(hyperlink), 'href')}""",
                     "Image": f"""{catchClause.attributes(content.query_selector(image), 'src')}""",
                 }
 
@@ -106,7 +111,7 @@ def amazonMe(head):
             try:
                 page.query_selector(next_button).click()
             except AttributeError:
-                print(f"Oops content loading error beyond this page. Issue on url {page.url} | number:> {click+1}")
+                print(f"Oops content loading error beyond this page. Issue on url {page.url} | number:> {click}")
                 break
 
         browser.close()
@@ -114,6 +119,6 @@ def amazonMe(head):
     print(f"Scraping done. Now exporting to excel database.")
 
     df = pd.DataFrame(amazon_dicts)
-    df.to_excel(f"Baby products 2-Amazon database.xlsx", index=False)
-    print(f"Baby products Database is saved.")
+    df.to_excel(f"{product_name}-Amazon database.xlsx", index=False)
+    print(f"{product_name} is saved.")
 

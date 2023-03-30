b91290c61f232a4652677463f59c9a93d025841f
sushil-rgb <sushil.bhandari002@gmail.com>
2023-03-31 04:26:40 +0545

    fixed

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import time
 sys.path.append(os.getcwd())
 from scrapers.scraper import Amazon
 
-  
-start_time = time.time()
+if __name__ == '__main__':   
+    start_time = time.time()
 
-# Make False if you want to see the live automation.
-make_headless = True
+    # Make False if you want to see the live automation.
+    make_headless = True
 
-print(Amazon().amazonMe(make_headless))
+    print(Amazon().amazonMe(make_headless))
 
-total_time = round(time.time()-start_time, 2)
-time_in_secs = round(total_time)
-time_in_mins = round(total_time/60)
+    total_time = round(time.time()-start_time, 2)
+    time_in_secs = round(total_time)
+    time_in_mins = round(total_time/60)
 
-print(f"Took {time_in_secs} seconds | {time_in_mins} minutes.")
+    print(f"Took {time_in_secs} seconds | {time_in_mins} minutes.")

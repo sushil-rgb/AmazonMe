6c62cf6ed1ba3d47df4edd7bdff2c32972ec5bdf
sushil-rgb <sushil.bhandari002@gmail.com>
2023-04-23 14:48:40 +0545

    fixed local var before assignment error

--- discordsFunctionalities/sendMessages.py
+++ discordsFunctionalities/sendMessages.py
@@ -20,17 +20,16 @@ async def getdataByasin(userInput, user):
     datas = await Amazon().dataByAsin(userInput)
     try:
         embed = discord.Embed(title=datas['Name'], url=datas['Hyperlink'], color=0xff9900)
+        embed.add_field(name = 'Price', value = datas['Price'], inline = False)
+        embed.add_field(name = 'Availability', value = datas['Availability'], inline = False)
+        embed.add_field(name = "Store", value = f"[{datas['Store']}]({datas['Store link']})", inline = False)
+        embed.add_field(name = 'Rating', value = datas['Rating'], inline = False)
+        embed.add_field(name ='Review count', value = datas['Rating count'], inline = False)
+        embed.set_thumbnail(url = datas['Image'])
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text = 'Powered by Python', icon_url = 'https://logos-download.com/wp-content/uploads/2016/10/Python_logo_icon.png')
+        await user.send(embed = embed)
     except TypeError:
-        await user.send("Content loading error. Please try again in few minutes.")
-    embed.add_field(name = 'Price', value = datas['Price'], inline = False)
-    embed.add_field(name = 'Availability', value = datas['Availability'], inline = False)
-    embed.add_field(name = "Store", value = f"[{datas['Store']}]({datas['Store link']})", inline = False)
-    embed.add_field(name = 'Rating', value = datas['Rating'], inline = False)
-    embed.add_field(name ='Review count', value = datas['Rating count'], inline = False)
-    embed.set_thumbnail(url = datas['Image'])
+        await user.send("Content loading error. Please try again in few minutes.")    
 
-    embed.timestamp = datetime.datetime.now()
-    embed.set_footer(text = 'Powered by Python', icon_url = 'https://logos-download.com/wp-content/uploads/2016/10/Python_logo_icon.png')
-
-    await user.send(embed = embed)
-
+    

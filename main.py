5a03d48244d0676358e69d9714c0d99e2378b40d
sushil-rgb <sushil.bhandari002@gmail.com>
2023-05-11 14:51:31 +0545

    minor repush

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     async def main():
         # While it is possible to reduce the time interval for faster scraping, I would advise against doing so as this could potentially overload the server and result in Amazon blocking your IP address.
         # Default time-interval ranges from (2 to 5 minutes). Scrape responsibly:
-        sleep = 5 * 60
+        sleep = 10 * 60
         
-        base_url = "https://www.amazon.com/s?k=Nintendo+Switch+Games&rh=n%3A16227133011&pf_rd_i=23508887011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=434db2ed-6d53-4c59-b173-e8cd550a2e4f&pf_rd_r=MG60RM4M4REFVTTRWYCZ&pf_rd_s=merchandised-search-5&pf_rd_t=101&ref=nb_sb_noss"
+        base_url = "https://www.amazon.com/s?k=Home&i=kitchen-intl-ship&crid=1QBODY970JKYC&pd_rd_r=b267967b-3f42-4fb9-99a3-c67d84414b1e&pd_rd_w=sRda0&pd_rd_wg=ysq5U&pf_rd_p=c9097eb6-837b-4ba7-94d7-51428f6e8d2a&pf_rd_r=C6SGM2A5M594847FZDVC&sprefix=home%2Ckitchen-intl-ship%2C164&ref=pd_gw_unk"
         
         amazon = Amazon()      
         datas = await amazon.concurrent_scraping(sleep, base_url)

91899944ea3163525f5ee196933160d91ca54d44
sushil-rgb <sushil.bhandari002@gmail.com>
2023-08-13 18:14:37 +0545

    fixed typo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         base_url = "https://www.amazon.com/s?i=toys-and-games&bbn=165793011&rh=n%3A165793011%2Cp_36%3A-3000&dc&_encoding=UTF8&content-id=amzn1.sym.eb39b83d-c690-496d-9f16-0a9bd66ca6c8&pd_rd_r=db10c08d-805f-4eb2-8215-6db7244dfd33&pd_rd_w=U10YV&pd_rd_wg=XWFTR&pf_rd_p=eb39b83d-c690-496d-9f16-0a9bd66ca6c8&pf_rd_r=1B8D3BHD1HB21JDP9X4B&qid=1663255924&rnid=386491011&ref=pd_gw_unk"
         status = await Amazon(base_url, None).status()
         if status == 503:
-            return "503 respsone. Please try again later."
+            return "503 response. Please try again later."
         # Type True if you want to export to CSV and avoid MongoDB
         csv = True
         # Type True if you want to use proxy:

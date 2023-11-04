7c76f2582e585d3efa5d752f993a1da4270a3e05
sushil-rgb <sushil.bhandari002@gmail.com>
2023-11-05 01:18:09 +0545

    first commit for actors

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-from mongo_database.mongo import export_to_mong
-from tools.tool import rand_proxies
+from apify import Actor
 from scrapers.scraper import Amazon
-import asyncio
-import time
+
 
 
 if __name__ == '__main__':
 
 
     async def main():
-        base_url = "https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.f9836480-cfbe-4ad3-ad9c-168436b5cd55&pd_rd_r=e7f0b407-5d50-4304-9834-e16d45350a0d&pd_rd_w=YT9MX&pd_rd_wg=7MvkN&pf_rd_p=f9836480-cfbe-4ad3-ad9c-168436b5cd55&pf_rd_r=Z128GHCSZH9Y2B5ZM15K&ref=pd_gw_unk"
-        status = await Amazon(base_url, None).status()
+        async with Actor:
+            input = await Actor.get_input()
+            status = await Amazon(input, None).status()
 
-        if status == 503:
-            return "503 response. Please try again later."
-        # Type True if you want to export to CSV and avoid MongoDB
-        csv = True
-        # Type True if you want to use proxy:
-        proxy = False
-        if csv:
-            if proxy:
-                amazon = Amazon(base_url, None)
-                return await amazon.export_csv()
+            if status == 503:
+                return '503 response. Please try again later.'
             else:
-                amazon = Amazon(base_url, f"http://{rand_proxies()}")
-                return await amazon.export_csv()
-        else:
-            if proxy:
-                mongo_to_db = await export_to_mong(base_url, f"http://{rand_proxies()}")
-            else:
-                mongo_to_db = await export_to_mong(base_url, None)
-            return mongo_to_db
+                amazon = Amazon(input, None)
+                datasets = await amazon.scrape_product_info(input['url'])
+                title = await amazon.category_name()
+                await Actor.push_data({'datas': datasets, 'title': title})
 
-
-    # Start the timer to measure how long the wb scraping process takes
-    start_time = time.time()
-    # Run the async main function and run the scraper:
-    results = asyncio.run(main())
-    end_time = time.time()
-    print(results)
-    # Calculate and print the total time taken to scrape the data:D
-    execution_time = round(end_time - start_time, 2)
-    print(f"Took {execution_time} seconds | {round(execution_time / 60, 2)} minutes.")
 

3baa8115b344e30ccfa5b740f49f9bc253236207
sushil-rgb <sushil.bhandari002@gmail.com>
2023-11-05 17:59:04 +0545

    test for datasets

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     async def main():
         async with Actor:
-            input = f'https://api.apify.com/v2/acts/{ACTOR_ID}/runs/last/dataset/items?token={APIFY_TOKENS}'
+            input = f'https://www.amazon.com/s?k=Jeans&rh=n%3A1040660%2Cn%3A1048188%2Cp_36%3A-5000&dc&ds=v1%3AZrh2YeJ%2Bmo6tc5p1QJD9idnCpBDTF2pKNTUbGqlhFKk&_encoding=UTF8&content-id=amzn1.sym.b0c3902d-ae70-4b80-8f54-4d0a3246745a&crid=1TZCO6ZC2HZVA&pd_rd_r=d2d812ec-1839-446b-b13c-080b49f2026c&pd_rd_w=SQ59S&pd_rd_wg=6taBx&pf_rd_p=b0c3902d-ae70-4b80-8f54-4d0a3246745a&pf_rd_r=MEEWY614NXBTSG4MW7R6&qid=1684823801&rnid=2941120011&sprefix=jeans%2Caps%2C155&ref=pd_gw_unk'
 
             status = await Amazon(input).status()
 
@@ -20,8 +20,8 @@ if __name__ == '__main__':
             else:
                 amazon = Amazon(input)
                 datasets = await amazon.concurrent_scraping()
-                title = await amazon.category_name()
-                await Actor.push_data({'Products': datasets, 'title': title})
+                # title = await amazon.category_name()
+                await Actor.push_data(datasets)
 
 
     print(asyncio.run(main()))

22100132aca175aeeb04921585222a1c2c16b049
sushil-rgb <sushil.bhandari002@gmail.com>
2023-11-05 23:59:16 +0545

    testing input

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from scrapers.scraper import Amazon
 from apify import Actor
 import asyncio
-import os
 
 
 if __name__ == '__main__':
@@ -10,13 +9,12 @@ if __name__ == '__main__':
     async def main():
         async with Actor:
             input_data = await Actor.get_input()
-            url = input_data[0]['url']['prefill']
-            status = await Amazon(url).status()
+            status = await Amazon(input_data).status()
 
             if status == 503:
                 return '503 response. Please try again later.'
             else:
-                amazon = Amazon(url)
+                amazon = Amazon(input_data)
                 datasets = await amazon.concurrent_scraping()
                 title = await amazon.category_name()
                 await Actor.push_data({'Products': datasets, 'title': title})

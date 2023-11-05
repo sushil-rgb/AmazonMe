d6f7d34a45ac68ff7c1d4cecd99dc7070018e0e7
sushil-rgb <sushil.bhandari002@gmail.com>
2023-11-06 01:45:42 +0545

    added try except clause

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ if __name__ == '__main__':
 
 
     async def main():
-        async with Actor:
-            input_data = await Actor.get_input()
-            url = input_data.get('url')
-            status = await Amazon(url).status()
+        try:
+            async with Actor:
+                input_data = await Actor.get_input()
+                url = input_data.get('url')
+                status = await Amazon(url).status()
 
-            if status == 503:
-                return '503 response. Please try again later.'
-            else:
-                amazon = Amazon(url)
-                datasets = await amazon.concurrent_scraping()
-                await Actor.push_data({'Products': datasets})
+                if status == 503:
+                    return '503 response. Please try again later.'
+                else:
+                    amazon = Amazon(url)
+                    datasets = await amazon.concurrent_scraping()
+                    await Actor.push_data({'Products': datasets})
+        except Exception as e:
+            print(f'WebSocket connection error: {e}')
+
 
     print(asyncio.run(main()))
 

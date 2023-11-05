de04c2386bd85599220bd9c5212ce546bbf8ae0b
sushil-rgb <sushil.bhandari002@gmail.com>
2023-11-06 00:16:51 +0545

    testing input

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
 
     async def main():
         async with Actor:
-            input_data = await Actor.get_input()
-            url = input_data.get('url')
+            # input_data = await Actor.get_input()
+            url = "https://www.amazon.com/s?k=toys&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.44da4965-9668-4613-bec2-a3a75f0c2ad4&pd_rd_r=948831c6-7837-4a26-a075-054f6ba6391e&pd_rd_w=rzz1M&pd_rd_wg=rtU6v&pf_rd_p=44da4965-9668-4613-bec2-a3a75f0c2ad4&pf_rd_r=2MRC4DNSXHB0YQ7YKDAZ&ref=pd_gw_unk"
             status = await Amazon(url).status()
 
             if status == 503:

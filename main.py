1cc6f53bcfcd21487db278af59bc9b688567c75a
sushil-rgb <sushil.bhandari002@gmail.com>
2024-01-02 13:51:23 +0545

    minor repush

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ if __name__ == '__main__':
 
 
     async def main():
-        try:
-            async with Actor:
-                input_data = await Actor.get_input()
-                url = input_data.get('url')
-                status = await Amazon(url).status()
+        async with Actor:
+            input_data = await Actor.get_input()
+            url = input_data.get('url')
+            status = await Amazon(url).status()
 
-                if status == 503:
-                    return '503 response. Please try again later.'
-                else:
-                    amazon = Amazon(url)
-                    datasets = await amazon.concurrent_scraping()
-                    await Actor.push_data({'Products': datasets})
-        except Exception as e:
-            print(f'WebSocket connection error: {e}')
+            if status == 503:
+                return '503 response. Please try again later.'
+            else:
+                amazon = Amazon(url)
+                datasets = await amazon.concurrent_scraping()
+                await Actor.push_data({'Products': datasets})
+
 
 
     print(asyncio.run(main()))
